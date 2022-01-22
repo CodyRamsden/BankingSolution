a5a98884a2e6b649e234423996ff8f8ebab06fa1
Cody Ramsden <ramsden1990@gmail.com>
2022-01-22 09:28:10 -0500

    Add project files.

---
+++ Banking/SavingsAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking {
+    public class SavingsAccount : Account {
+
+        public decimal InterestRate { get; set; } = .12m;
+
+        public decimal CalculateAndPayInterest(int MonthsToPay) {
+            var Interest = InterestRate / 12 * MonthsToPay * Balance;
+            Deposit(Interest);
+            return Interest;
+        }
+
+    }
+}
